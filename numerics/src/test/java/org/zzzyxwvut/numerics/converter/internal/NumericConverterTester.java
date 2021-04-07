c22cba9f8e2ad72b13d1c49c767c85dcf8053d60
Aliaksei Budavei <oytis@yandex.ru>
2021-04-07 23:42:11 +0300

    Create a NumericConverterTester class

---
+++ numerics/src/test/java/org/zzzyxwvut/numerics/converter/internal/NumericConverterTester.java
@@ -0,0 +1,34 @@
+package org.zzzyxwvut.numerics.converter.internal;
+
+import java.util.Set;
+
+import org.zzzyxwvut.ate.Testable;
+import org.zzzyxwvut.ate.Tester;
+import org.zzzyxwvut.ate.configuration.Configurable;
+
+import org.zzzyxwvut.numerics.converter.ArabicRomanConverterTests;
+
+class NumericConverterTester
+{
+	static {
+		final ClassLoader loader = NumericConverterTester.class
+			.getClassLoader();
+		loader.setClassAssertionStatus(
+			"org.zzzyxwvut.numerics.converter.ArabicRomanConverterTests",
+									true);
+	}
+
+	private NumericConverterTester() { /* No instantiation. */ }
+
+	public static void main(String... args)
+	{
+		Tester.newBuilder(Set.of(ArabicRomanConverterTests.class))
+			.configurable(Configurable.newBuilder()
+				.executionPolicy(Testable.ExecutionPolicy
+								.CONCURRENT)
+				.build())
+			.verifyAssertion()
+			.build()
+			.runAndReport();
+	}
+}
