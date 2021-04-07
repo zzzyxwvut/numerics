2ab0c3712c014b42e7224c55f5b06f3de51538a6
Aliaksei Budavei <oytis@yandex.ru>
2021-04-07 22:52:47 +0300

    Add the ate optional dependency

--- numerics/src/main/java/module-info.java
+++ numerics/src/main/java/module-info.java
@@ -1,5 +1,8 @@
 /** Defines conversion for some numerics. */
 module org.zzzyxwvut.numerics.converter
 {
+	/* For IDE'd peace of mind. */
+	requires static org.zzzyxwvut.ate;
+
 	exports org.zzzyxwvut.numerics.converter;
 }
