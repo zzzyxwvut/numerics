fa6f210d789bed8d90c52086c225f62d3530a316
Aliaksei Budavei <oytis@yandex.ru>
2021-10-14 18:22:22 +0300

    Admit private access for inter-modular deep reflection
    (See https://bugs.openjdk.java.net/browse/JDK-8228624.)

--- numerics/src/test/java/org/zzzyxwvut/numerics/converter/internal/TestUplooker.java
+++ numerics/src/test/java/org/zzzyxwvut/numerics/converter/internal/TestUplooker.java
@@ -1,5 +1,18 @@
 package org.zzzyxwvut.numerics.converter.internal;
 
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodHandles;
+
 import org.zzzyxwvut.ate.support.AccessibleUplooker;
 
-public class TestUplooker extends AccessibleUplooker { }
+/** An {@code AccessibleUplooker}. */
+public class TestUplooker extends AccessibleUplooker
+{
+	/* See https://bugs.openjdk.java.net/browse/JDK-8228624. */
+	@Override
+	public Lookup exportableLookup()
+	{
+		return MethodHandles.lookup()
+			.dropLookupMode(Lookup.PROTECTED);
+	}
+}
