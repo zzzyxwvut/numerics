d49a1107187da109adf3f7dbdb9e580ed4cfe0f5
Aliaksei Budavei <0x000c70@gmail.com>
2023-05-05 22:08:20 +0300

    Improve documentation for Converter

--- numerics/src/main/java/org/zzzyxwvut/numerics/converter/internal/Converter.java
+++ numerics/src/main/java/org/zzzyxwvut/numerics/converter/internal/Converter.java
@@ -3,20 +3,20 @@ package org.zzzyxwvut.numerics.converter.internal;
 import java.util.function.Supplier;
 
 /**
- * This interface declares conversion between some types.
+ * This interface declares conversion between suppliers.
  *
- * @param <A> the type of an unwrapped object to convert from
- * @param <T> the type of an object to convert from
- * @param <B> the type of an unwrapped object to convert to
- * @param <U> the type of an object to convert to
+ * @param <A> the type of a supplier-furnished object to convert from
+ * @param <T> the type of a supplier to convert from
+ * @param <B> the type of a supplier-furnished object to convert to
+ * @param <U> the type of a supplier to convert to
  */
 interface Converter<A, T extends Supplier<A>, B, U extends Supplier<B>>
 {
 	/**
-	 * Returns an object converted from the passed value.
+	 * Returns a supplier converted from the passed supplier.
 	 *
-	 * @param value a value
-	 * @return an object converted from the passed value
+	 * @param supplier a supplier
+	 * @return a supplier converted from the passed supplier
 	 */
-	U convert(T value);
+	U convert(T supplier);
 }
